ce78f955b6acacaff8e6c9c18bfc32e4783e95dc
ChetanDighole <digholechetan@gmail.com>
2022-12-06 02:04:28 +0530

    initial commit

---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+
+const NavBar = () => {
+  return (          
+    <nav>
+        <ul>
+            <li>
+                <NavLink to="/">Home</NavLink>
+            </li>
+            <li>
+                <NavLink to="/about">About</NavLink>
+                
+            </li>
+            <li>
+                <NavLink to="/services">Services</NavLink>
+            </li>
+        </ul>
+    </nav>
+  )         
+}
+
+export default NavBar           
